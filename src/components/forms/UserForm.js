606220feb1dfd158ee5831e9490d16ccc7ae8925
Galang, Jason Clark L <jason.clark.l.galang@accenture.com>
2020-07-10 16:45:29 +0800

    first commit

---
+++ src/components/forms/UserForm.js
@@ -0,0 +1,81 @@
+import React, { useState, Fragment } from 'react'
+import AddUserForm from '../../components/forms/AddUserForm'
+import EditUserForm from '../../components/forms/EditUserForm'
+import UserTable from '../tables/UserTable';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const UserForm = () => {
+	const initialState = { 
+		id: null, 
+		name: '', 
+		email: '', 
+		contact: '' 
+    }
+    
+	// Setting state
+	const [ users, setUsers ] = useState('')
+	const [ existingUser, setExistingUser ] = useState(initialState)
+	const [ editUser, setEditUser ] = useState(false)
+	const [isLoading, setIsLoading] = useState(false);
+
+	console.log("Renderingz");
+
+	// CRUD operations
+	const addUser = user => {
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
+
+	const deleteUser = id => {
+		setEditUser(false)
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setEditUser(false)
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editRow = user => {
+		setEditUser(true)
+		setExistingUser({ id: user.id, name: user.name, email: user.email, contact: user.contact })
+	}
+
+	/* const viewData = (user, id) => {
+		set
+	} */
+
+	return (
+		<div className="container mt-4">
+			<div className="flex-column">
+				<div className="flex-large">
+					{editUser ? (
+						<Fragment>
+							
+							<EditUserForm
+								edit={editUser}
+								setEdit={setEditUser}
+								existingUser={existingUser}
+								updateUser={updateUser}
+							/>
+						</Fragment>
+					) : (
+						<Fragment>
+							<AddUserForm addUser={addUser} />
+						</Fragment>
+					)}
+				</div>
+				<div className="flex-small mt-4">
+					<UserTable 
+					users={users} 
+					editRow={editRow} 
+					deleteUser={deleteUser}
+					 />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default UserForm;
---
+++ src/components/tables/UserTable.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const UserTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Contact</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.contact}</td>
+            <td>
+              <button
+                onClick={() => {
+                  props.editRow(user)
+                }}
+                className="button btn-secondary"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => {
+                  props.editRow(user)
+                }}
+                className="button btn-info"
+              >
+                View
+              </button>
+              <button
+                onClick={() => props.deleteUser(user.id)}
+                className="button btn-danger"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={4}>Please add a new contact </td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
+
+export default UserTable;
